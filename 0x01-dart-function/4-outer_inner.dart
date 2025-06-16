ee02fa05ef85065827ea1cfc2f850ff6d38c6a5b
Gabriel <fossecruor@gmail.com>
2025-06-16 13:08:35 +0200

    fix: i cannot read ..

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,7 +1,8 @@
 
 void outer(String name, String id) {
   String inner() {
-    return "Hello $name your id is $id";
+    List<String> names = name.split(' ');
+    return "Hello Agent ${names.last.substring(0,1)}.${names.first} your id is $id";
   }
   print(inner());
 }
