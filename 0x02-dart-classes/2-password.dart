f2a6e516f5014ed1293d20d2b8f9b817ed85fb43
Gabriel <fossecruor@gmail.com>
2025-06-20 14:44:27 +0200

    add password class

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,14 @@
+class Password {
+  String password = "";
+
+  @override
+  String toString() {
+    return 'Your Password is: ${this.password}';
+  }
+  bool isValid() {
+    if (this.password.length >= 8 && this.password.length <= 16 && this.password.contains(RegExp(r'[a-zA-Z]+|\d+'))) {
+      return true;
+    }
+    return false;
+  }
+}
