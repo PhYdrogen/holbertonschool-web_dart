b17c2535763aab66db094cf64d53acb0748db222
Gabriel <fossecruor@gmail.com>
2025-06-20 14:49:44 +0200

    fix: 2 and add private on task 3

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -5,8 +5,12 @@ class Password {
   String toString() {
     return 'Your Password is: ${this.password}';
   }
+
   bool isValid() {
-    if (this.password.length >= 8 && this.password.length <= 16 && this.password.contains(RegExp(r'[a-zA-Z]+|\d+'))) {
+    if (this.password.length >= 8 &&
+        this.password.length <= 16 &&
+        this.password.contains(RegExp(r'[a-zA-Z]+')) &&
+        this.password.contains(RegExp(r'\d+'))) {
       return true;
     }
     return false;
