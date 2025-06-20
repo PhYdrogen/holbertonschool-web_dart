87835d2826d6ed59e12b9864228cbab7a6a911d7
Gabriel <fossecruor@gmail.com>
2025-06-20 14:55:54 +0200

    add mutable as properties

---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,29 @@
+class Password {
+  String _password = "";
+
+  void set password(String pwd) {
+    this._password = pwd;
+  }
+  String get password {
+    return this._password;
+  }
+
+  Password({required password}) {
+    this._password = password;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: ${this._password}';
+  }
+
+  bool isValid() {
+    if (this._password.length >= 8 &&
+        this._password.length <= 16 &&
+        this._password.contains(RegExp(r'[a-zA-Z]+')) &&
+        this._password.contains(RegExp(r'\d+'))) {
+      return true;
+    }
+    return false;
+  }
+}
