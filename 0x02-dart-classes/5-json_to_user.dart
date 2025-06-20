4f58da23d5dc2364fb700842c57e6f550686de06
Gabriel <fossecruor@gmail.com>
2025-06-20 15:02:46 +0200

    add basic JSONify method

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,26 @@
+class User {
+  int? id;
+  String? name;
+  int? age;
+  double? height;
+
+  User({required id, required String name,required int age,required double height}) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.height = height;
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'id': this.id, 'name': this.name, 'age': this.age, 'height': this.height};
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(id: userJson['id'], name: userJson['name'], age: userJson['age'], height: userJson['height']);
+  }
+
+  @override
+  String toString() {
+    return 'User(id : ${this.id} ,name: ${this.name}, age: ${this.age}, height: ${this.height}';
+  }
+}
